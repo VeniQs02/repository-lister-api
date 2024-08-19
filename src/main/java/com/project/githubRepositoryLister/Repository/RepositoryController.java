093ab50de5c70fd5079d14c42b9c7443a7a973a4
Kubus <jakub.kangowski7@vp.pl>
2024-08-19 12:49:05 +0200

    version 1.41a > minor warning fix

--- src/main/java/com/project/githubRepositoryLister/Repository/RepositoryController.java
+++ src/main/java/com/project/githubRepositoryLister/Repository/RepositoryController.java
@@ -2,7 +2,6 @@ package com.project.githubRepositoryLister.Repository;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
